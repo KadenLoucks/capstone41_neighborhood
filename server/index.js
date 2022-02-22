b22b2ebbe581ee7275055c5f27fc7146ab89887e
Haomin Chen <chehaomi@sheridancollege.ca>
2022-02-22 15:16:59 -0500

    modify connection config

--- server/index.js
+++ server/index.js
@@ -1,21 +1,18 @@
 import express from "express";
-import bodyPaser from "body-parse";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
 //init app
 const app=express();
 
-app.use(bodyPaser.json({limit:"30mb",extended:true}));
-app.use(bodyPaser.urlencoded({limit:"30mb",extended:true}));
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 
-//connection string into MongoDB
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://haomin:haomin@minineighbourhood.krps8.mongodb.net/MiniNeighbourhood?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+//connect app with database
+const CONNECTION_URL = "mongodb+srv://haomin:haomin@minineighbourhood.krps8.mongodb.net/miniNeighbourhood?retryWrites=true&w=majority";
+const PORT=process.env.PORT||5000;
+mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>app.listen(PORT,()=>console.log('server running on port:'+PORT)))
+.catch((error)=>console.log(error.message));
