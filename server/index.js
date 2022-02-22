374dc02fae706e70c40549b8abe0ae19556e754d
Haomin Chen <chehaomi@sheridancollege.ca>
2022-02-22 14:35:05 -0500

    config connection with database

--- server/index.js
+++ server/index.js
@@ -9,3 +9,13 @@ const app=express();
 app.use(bodyPaser.json({limit:"30mb",extended:true}));
 app.use(bodyPaser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+
+//connection string into MongoDB
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://haomin:haomin@minineighbourhood.krps8.mongodb.net/MiniNeighbourhood?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
