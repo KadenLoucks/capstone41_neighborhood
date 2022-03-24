0337e69e55bd15a3831e5a541303b8636e1453cc
HaominChen <chehaomi@sheridancollege.ca>
2022-03-23 20:02:33 -0400

    Add Redux

--- client/src/app.js
+++ client/src/app.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React ,{useEffect} from "react";
 import {Container, AppBar,Typography,Grow,Grid} from '@material-ui/core';
-
+import { useDispatch } from "react-redux";
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
-
+import {getPosts} from './actions/posts';
 import memories from './images/memories.png';
 import useStyles from './styles';
 
 const App=()=>{
     const classes = useStyles();
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getPosts());
+    },[dispatch]);
     return (
         <Container maxidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ import postRoutes from './routes/posts.js';
 
 //init app
 const app=express();
-app.use('/posts', postRoutes);
+
 
 app.use(bodyParser.json({limit:"30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
-
+app.use('/posts', postRoutes);
 //connect app with database
 const CONNECTION_URL = "mongodb+srv://kaden:kaden@minineighbourhood.krps8.mongodb.net/miniNeighbourhood?retryWrites=true&w=majority";
 const PORT=process.env.PORT||5000;
