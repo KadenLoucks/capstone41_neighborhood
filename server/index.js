c57b9c2d2542f9f4d9fb644b60bcdef07b0b2588
Qian Li <liqian@sheridancollege.ca>
2022-03-23 21:15:37 -0400

    commit 1

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
 
-//init app
-const app=express();
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
 
-app.use(bodyParser.json({limit:"30mb",extended:true}));
-app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-//connect app with database
-const CONNECTION_URL = "mongodb+srv://qianli:qianli@minineighbourhood.krps8.mongodb.net/miniNeighbourhood?retryWrites=true&w=majority";
-const PORT=process.env.PORT||5000;
-mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
-.then(()=>app.listen(PORT,()=>console.log('server running on port:'+PORT)))
-.catch((error)=>console.log(error.message));
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://qianli:qianli@minineighbourhood.krps8.mongodb.net/miniNeighbourhood?authSource=admin&replicaSet=atlas-a736tq-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
